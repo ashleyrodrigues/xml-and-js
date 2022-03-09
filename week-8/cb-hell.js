820b5e8cc8cf987d086aeeb28d79006d014ae173
ashleyrodrigues <rodriguesash510@gmail.com>
2022-03-08 20:35:58 -0500

    comits

--- week-8/cb-hell.js
+++ week-8/cb-hell.js
@@ -3,53 +3,30 @@
  * 2. Fix callback hell by rewriting it with async/await
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
+ const timeout = (ms) => new Promise(resolve => {
+  setTimeout(resolve, ms);
+})
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
-}
+const processData = async (data, converterFn) =>
+  await Promise.all(data.map(async (value) => {
+      return { val1: value, res1: await converterFn(value) };
+  }))
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
+  const generateRandomNumber = () => Math.floor(Math.random() * 40);
+  const generateData = async () => timeout(2000).then(() => Array.from({ length: 20 }, generateRandomNumber))
+  const convertToFeet = (meters) => timeout(4000).then(() => (meters * 3.2808))
 
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
 
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
-}
+const logResult = (meters, feet) => console.log(`Converted ${meters}m to ${feet}ft`);
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
 
-function logResult(meters, feet) {
-  console.log(`Converted ${meters}m to ${feet}ft`);
-}
-
-function main() {
+const main = async () => {
   console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
-    });
-  });
+  let data = await generateData()
+  let processedData = await processData(data, convertToFeet)
+  processedData.forEach(element => {
+      logResult(element.val1, element.res1)
+  })
   console.log("Finish");
 }
 
--- week-8/sync.js
+++ week-8/sync.js
@@ -1,19 +1,22 @@
-function inc(a) {
-  return a + 1;
+function timeout(){
+  return new Promise((resolve) => setTimeout(resolve, 3000));
 }
 
-const sum = function (a, b) {
-  return a + b;
+async function inc(a) {
+  return new Promise((resolve) => timeout(3000).then(() => resolve(a + 1))); 
+}
+
+ const sum = function (a, b) {
+  return  new Promise((resolve) => timeout(3000).then(() => resolve(a + b)));
 };
 
 const max = (a, b) => (a > b ? a : b);
 
-const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
+ const avg = (a, b) => {
+  return  new Promise((resolve) => timeout(3000).then(() => sum(a,b)).then((s) => resolve(s/2)));
 };
-
-const obj = {
+``
+ const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
     return this.name.split(sep);
@@ -24,11 +27,9 @@ class Person {
   constructor(name) {
     this.name = name;
   }
-
   static of(name) {
     return new Person(name);
   }
-
   split(sep = " ") {
     return this.name.split(sep);
   }
@@ -36,9 +37,14 @@ class Person {
 
 const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
+
+async function print() {
+console.log("inc(5) =", await inc(5));
+console.log("sum(1, 3) =",await sum(1, 3));
+console.log("max(8, 6) =",await max(8, 6));
+console.log("avg(8, 6) =",await avg(8, 6));
 console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+console.log("person.split() =", person.split())
+}
+
+print();
